49ca5bb14d33d8c8f651cd4ac407079826e024a8
Euteec <91482084+Euteec@users.noreply.github.com>
2021-09-27 18:13:27 +0300

    Add files via upload

---
+++ ACTIVE_BUZZER.ino
@@ -0,0 +1,27 @@
+int buzzpin=7;
+int mynumber;
+int potval=A0;
+int val;
+String msg="potentiometer value";
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzpin,OUTPUT);
+pinMode(potval,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val=analogRead(potval);
+Serial.println(val);
+delay(500);
+if (val>=800)
+{
+  digitalWrite(buzzpin,HIGH);
+  delay(200);
+  
+}
+else{
+  digitalWrite(buzzpin,LOW);
+}
+}
